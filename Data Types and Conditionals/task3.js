0e66acca7245984aa557d99db90fcc8f62f1f55b
sandi <satrianindhita6@gmailcom>
2024-08-07 01:52:33 +0700

    docs: :memo: complete Data Type and Conditionals in js

--- Data Types and Conditionals/task3.js
+++ Data Types and Conditionals/task3.js
@@ -9,6 +9,9 @@ if (a + b < 4) {
 let a = 3;
 let b = 2;
 let result;
+
+result = (a + b < 4) ? 'Below' : 'Over';
+////////////////////////////////
 (a + b < 4) ? result = 'Below' : result = 'Over';
 
 console.log(result); // Output: 'Over'
